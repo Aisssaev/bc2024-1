a65b6627d2438f456742b783af47477cf8d5281f
Aisssaev bc2024-1 <miroslavzizyn2006@gmail.com>
2024-09-05 18:13:37 +0300

    Changed logic

--- hello.js
+++ hello.js
@@ -1,2 +1,13 @@
-const name = user.name
-console.log(name)
+const { exec } = require('child_process');
+
+exec('git config --global user.name', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Error out: ${stderr}`);
+        return;
+    }
+    console.log(`User name in Git: ${stdout.trim()}`);
+});
