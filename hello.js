2787b6d2217719480ef6db31165034116e85b771
Aisssaev bc2024-1 <miroslavzizyn2006@gmail.com>
2024-09-05 18:50:02 +0300

    1

--- hello.js
+++ hello.js
@@ -1,13 +1,5 @@
 const { exec } = require('child_process');
 
 exec('git config --global user.name', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Error out: ${stderr}`);
-        return;
-    }
     console.log(`User name in Git: ${stdout.trim()}`);
 });
