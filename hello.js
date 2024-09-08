8c2f91a3e1c227b9bb181c23f3b6908e5f6f21c8
Aisssaev bc2024-1 <miroslavzizyn2006@gmail.com>
2024-09-08 19:03:57 +0300

    Changed function exec

--- hello.js
+++ hello.js
@@ -1,5 +1,17 @@
-const { exec } = require('child_process');
+const { exec } = require('child_process');//exec - функція з модуля child_process
 
+//const child_process = require('child_process');
+//const exec = child_process.exec;
+
+//Виконуэмо команду, яка вертає ім'я користувача Git
 exec('git config --global user.name', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Error out: ${stderr}`);
+        return;
+    }
     console.log(`User name in Git: ${stdout.trim()}`);
 });
