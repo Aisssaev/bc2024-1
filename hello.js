11f06164ca40f754cb8a20c426c2d36ed683f5f7
Aisssaev <miroslavzizyn2006@gmail.com>
2024-09-14 10:58:09 +0300

    fix

--- hello.js
+++ hello.js
@@ -1,17 +1,2 @@
-const { exec } = require('child_process');//exec - функція з модуля child_process
-
-//const child_process = require('child_process');
-//const exec = child_process.exec;
-
-//Виконуэмо команду, яка вертає ім'я користувача Git
-exec('git config --get user.name', (error, stdout, stderr) => {
-    if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.error(`Error out: ${stderr}`);
-        return;
-    }
-    console.log(`Hello, ${stdout.trim()}`);
-});
+var username = "Aisssaev"
+console.log("Hello,", username)
